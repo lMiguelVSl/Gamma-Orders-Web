fb4908065e566ea9d88c8b44636f5a19d81ec772
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-09-30 16:36:55 -0500

    order view authorization

--- src/app/components/table-orders/table-orders.component.ts
+++ src/app/components/table-orders/table-orders.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseModule } from '../../shared/base/base.module';
 import { OrderService } from '../../services/order/order.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Order } from '../../services/order/models';
 import { filter, take } from 'rxjs';
 
@@ -16,10 +16,13 @@ export class TableOrdersComponent implements OnInit {
 
   displayedColumns: string[] = ['Order ID', 'N.Dishes', 'Status'];
   dataSource: Order[] = [];
+  authenticated: boolean = false;
 
-  constructor(private orderService: OrderService, private route: ActivatedRoute) { }
+  constructor(private orderService: OrderService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    sessionStorage.getItem('userId') ? this.authenticated = true : this.authenticated = false;
+    if(!this.authenticated) this.router.navigate(['/NotAuthorized']);
     const userId = this.route.snapshot.paramMap.get('idUser');
     if (userId) this.orderService.getOrdersByClientId(userId)
       .pipe(
